e73f44388a830711bae0dfc0fd58c877fdfd1d2c
Erik Lax <erik@halon.se>
2015-08-26 09:41:35 +0200

    First commit for a cPanel session-transfer plugin

---
+++ cpanel-sp-enduser.live.php
@@ -0,0 +1,61 @@
+<?php
+// Non-cPanel users should use the regular version instead
+if (!isset($_SERVER['CPANEL']) || $_SERVER['CPANEL'] != 'active')
+	die();
+
+// Get timezone offset from client
+if (isset($_GET['timezone'])) setcookie('timezone', intval($_GET['timezone']));
+if (!isset($_COOKIE['timezone']))
+	die('<script>window.location.href = "?timezone=" + new Date().getTimezoneOffset();</script>');
+
+// Cpanel API
+require_once '/usr/local/cpanel/php/cpanel.php';
+$cpanel = &new CPANEL();
+
+// For some reason, querying 'listpops' when signed in as a domain
+// owner returns only the username, while logging in as a specific
+// email address returns that address (and likely aliases as well)
+if(strpos($_SERVER['REMOTE_USER'], '@') === false)
+{
+	// It's the domain owner, give them access to everything
+	$domains_res = $cpanel->api2('Email', 'listmaildomains');
+	$domains = array();
+	foreach($domains_res['cpanelresult']['data'] as $data)
+		$domains[] = $data['domain'];
+	if(empty($domains))
+		die("No Domains");
+	$access = array('domain' => $domains);
+}
+else
+{
+	// It's an email user, give them access to their own account
+	$addresses_res = $cpanel->api2('Email', 'listpops');
+	$addresses = array();
+	foreach($addresses_res['cpanelresult']['data'] as $data)
+		$addresses[] = $data['email'];
+	if(empty($addresses))
+		die("No Addresses");
+	$access = array('mail' => $addresses);
+}
+
+require_once 'settings.php';
+
+$get = http_build_query(
+		array(
+			'username' => $_SERVER['REMOTE_USER'],
+			'api-key' => $apikey
+		)
+	);
+$opts = array('http' =>
+	array(
+		'method'  => 'POST',
+		'header'  => 'Content-type: application/x-www-form-urlencoded',
+		'content' => http_build_query($access) 
+	)
+);
+$context = stream_context_create($opts);
+$result = json_decode(@file_get_contents($enduser.'session-transfer.php?'.$get, false, $context));
+if (!$result || !isset($result->session))
+	die('Transfer failed');
+
+header('Location: '.$enduser.'session-transfer.php?session='.$result->session);
---
+++ settings.php
@@ -0,0 +1,4 @@
+<?php
+
+$enduser = 'http://10.2.0.166/enduser/';
+$apikey = 'secret';
